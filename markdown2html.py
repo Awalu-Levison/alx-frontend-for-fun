6957a90a260e4710dfd5a0efcb6a9da1694a1b27
Awalu-Levison <levisonawalu251@gmail.com>
2024-10-19 04:05:59 +0000

    Updated all files

--- markdown2html.py
+++ markdown2html.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 """Python interpreter shell"""
 
+
+import sys
+"""Imported the system module
+to handle commandline arguments"""
+
 file_name = sys.argv[1]
+out_put = sys.argv[2]
+
+if len(sys.argv) < 3:
+    ii
